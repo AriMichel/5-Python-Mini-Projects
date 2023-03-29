58e726cb30dc0411963eccd6510847fbf9e95271
AriMichel <ari.michel.vu@gmail.com>
2023-03-29 21:22:44 +0200

    Program 1 done

---
+++ 1_quiz_game.py
@@ -0,0 +1,56 @@
+
+print('''
+ ____________________
+!                    !
+!      Welcome       !
+!    to  QUICK-QUIZ  !
+\____________________/
+         !  !
+         L_ !
+        / _)!
+       / /__L
+ _____/ (____)
+        (____)
+ _____  (____)
+      \_(____)
+         !  !
+         \__/   ''')
+playing= input("Do you want to play? (y/n) ")
+
+if playing.lower() != "y":
+    print("See you around!")
+    quit()
+
+print("Let's PLAY! :)")
+score = 0 
+
+answer = input("What does CPU stand for? ")
+if answer.lower() =="central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong! CPU stands for central processing unit")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() =="graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong! GPU stands for graphics processing unit")
+
+answer = input("What does RAM stand for? ").lower()
+if answer =="random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong! GPU stands for random access memory")
+
+answer = input("What does PSU stand for? ").lower()
+if answer =="power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong! GPU stands for power supply")
+
+print("You got " + str(score) + " questions correct!")
+print(f"Your score is {((score/4) * 100)}%")
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # 5-Python-Mini-Projects
 5 Python Mini Projects
+
+
+We are going to code 5 games/programs, perfect projects for beginners. 
+
+- Quiz game: The idea is to give the user a couple of questions and for every correct answer we will add 1 point to their score
+
+- Number Guessing Game
+
+- Rock, Paper, Schissors
+
+- Choose your own adventure
+
+- Password Manager
+
+
